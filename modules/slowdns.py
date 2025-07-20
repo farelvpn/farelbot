1270c721555fb048f9cc18f2dad98036d6800e5a
Farell Aditya Ardian <farell@rerechanstore.eu.org>
2025-07-20 15:38:20 +0700

    Create slowdns.py

---
+++ modules/slowdns.py
@@ -0,0 +1,19 @@
+from farelbot import *
+
+@bot.on(events.CallbackQuery(data=b'slowdns_menu'))
+async def show_slowdns_menu_placeholder(event):
+    """
+    Menampilkan menu placeholder untuk SlowDNS.
+    """
+    if valid(str(event.sender_id)) != "true":
+        return await event.answer("Akses Ditolak!", alert=True)
+
+    # Pesan yang memberitahukan bahwa fitur ini belum tersedia
+    menu_text = "╭─ **MENU SLOWDNS** ─╮\n│\n├─ 🚧 Fitur ini sedang dalam\n│  pengembangan dan akan\n│  segera hadir.\n│\n╰─ (Oleh: @farellvpn) ─╯"
+    
+    # Tombol untuk kembali ke menu pengaturan
+    buttons = [
+        [Button.inline("« Kembali ke Pengaturan", "setting")]
+    ]
+
+    await event.edit(menu_text, buttons=buttons)
